6034c32ab4efdae9a1ba2b65b7b16dfe513ce303
jyk <422925090@qq.com>
2017-10-29 00:44:58 +0800

    finish manage functions

--- app/manage/views.py
+++ app/manage/views.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, redirect, request, flash
 from flask_login import login_required, current_user
 from . import manage
 from .. import db
-from ..models import User, Blog
+from ..models import User, Blog, Comment
 from ..decorators import admin_required
 from .forms import ChangePasswordForm, BlogForm
 
@@ -22,7 +22,19 @@ def change_password():
             flash('密码错误')
     return render_template('manage/change_password.html', form=form)
 
-# 创建博客
+# 文章管理
+@manage.route('/blogs')
+@admin_required
+@login_required
+def manage_blogs():
+    page = request.args.get('page', 1, type=int)
+    pagination = Blog.query.order_by(Blog.create_at.desc()).paginate(
+        page, per_page=10, error_out=False)
+    blogs = pagination.items
+    return render_template('manage/manage_blogs.html', blogs=blogs, 
+                            pagination=pagination)
+
+# 写文章
 @manage.route('/create-blog', methods = ['GET', 'POST'])
 @admin_required
 @login_required
@@ -37,6 +49,7 @@ def create_blog():
         return redirect(url_for('main.blog', id=blog.id))
     return render_template('manage/create_blog.html', form=form)
 
+# 编辑文章
 @manage.route('/edit-blog/<int:id>', methods = ['GET', 'POST'])
 @admin_required
 @login_required
@@ -53,4 +66,43 @@ def edit_blog(id):
     form.name.data = blog.name
     form.summary.data = blog.summary
     form.content.data = blog.content
-    return render_template('manage/create_blog.html', form=form)
+    return render_template('manage/edit_blog.html', form=form)
+
+# 删除文章
+@manage.route('/delete-blog/<int:id>')
+@admin_required
+@login_required
+def delete_blog(id):
+    blog = Blog.query.get_or_404(id)
+    db.session.delete(blog)
+    db.session.commit()
+    flash('文章已删除')
+    return redirect(url_for('manage.manage_blogs'))
+
+# 评论管理
+@manage.route('/comments')
+@admin_required
+@login_required
+def manage_comments():
+    page = request.args.get('page', 1, type=int)
+    pagination = Comment.query.order_by(Comment.create_at.desc()).paginate(
+        page, per_page=10, error_out=False)
+    comments = pagination.items
+    return render_template('manage/manage_comments.html', comments=comments, 
+                            pagination=pagination)
+
+# 屏蔽评论
+
+
+
+# 用户管理
+@manage.route('/users')
+@admin_required
+@login_required
+def manage_users():
+    page = request.args.get('page', 1, type=int)
+    pagination = User.query.order_by(User.create_at.desc()).paginate(
+        page, per_page=10, error_out=False)
+    users = pagination.items
+    return render_template('manage/manage_users.html', users=users, 
+                            pagination=pagination)
--- app/models.py
+++ app/models.py
@@ -3,9 +3,9 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, LoginManager, current_user, AnonymousUserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app
+from flask import current_app, request
 from markdown import markdown
-import bleach
+import bleach, hashlib
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -15,10 +15,15 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index = True)
     password_hash = db.Column(db.String(128))
     admin = db.Column(db.Boolean, default=False)
-    image = db.Column(db.String(64))
+    avatar_hash = db.Column(db.String(64))
     create_at = db.Column(db.DateTime, default=datetime.utcnow)
     blogs = db.relationship('Blog', backref='author', lazy= 'dynamic')
     comments = db.relationship('Comment', backref='author', lazy= 'dynamic')
+
+    def __init__(self, **kw):
+        super(User, self).__init__(**kw)
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
 
     @property
     def password(self):
@@ -33,6 +38,34 @@ class User(UserMixin, db.Model):
 
     def is_administrator(self):
         return self.admin
+
+    # 生成Gravatar头像
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+        hash = self.avatar_hash or hashlib.mp5(self.email.encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+            url=url, hash=hash, size=size, default=default, rating=rating)
+
+    @staticmethod
+    def generate_fake(count=100):
+        from sqlalchemy.exc import IntegrityError
+        from random import seed
+        import forgery_py
+
+        seed()
+        for i in range(count):
+            u = User(email = forgery_py.internet.email_address(),
+                     username = forgery_py.internet.user_name(True),
+                     password = forgery_py.lorem_ipsum.word(),
+                     admin = False)
+            db.session.add(u)
+            try:
+                db.session.commit()
+            except IntegrityError:
+                db.session.rollback()
 
 class AnonymousUser(AnonymousUserMixin):
     def is_administrator(self):
@@ -75,6 +108,26 @@ class Blog(db.Model):
             markdown(value, output_format = 'html'), 
             tags = allowed_tags, strip = True))
 
+    @staticmethod
+    def generate_fake(count=100):
+        from random import seed, randint
+        import forgery_py
+        u = User.query.filter_by(id=1).first()
+
+        seed()
+        for i in range(count):
+            try:
+                b = Blog(name = forgery_py.lorem_ipsum.sentences(1),
+                         summary = forgery_py.lorem_ipsum.sentences(randint(3,5)),
+                         content = forgery_py.lorem_ipsum.sentences(randint(8,10)),
+                         create_at = forgery_py.date.date(True),
+                         author = u)
+                db.session.add(b)
+                db.session.commit()
+            except AttributeError:
+                pass    
+
+
 
 db.event.listen(Blog.summary, 'set', Blog.on_changed_summary)
 db.event.listen(Blog.content, 'set', Blog.on_changed_content)
@@ -83,10 +136,27 @@ class Comment(db.Model):
     __tablename__ = 'comments'
 
     id = db.Column(db.Integer, primary_key=True)
-    diabled = db.Column(db.Boolean)
     content = db.Column(db.Text)
     create_at = db.Column(db.DateTime, default=datetime.utcnow, index = True)
+    disabled = db.Column(db.Boolean, default=False)
     blog_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     author_id = db.Column(db.Integer, db.ForeignKey('blogs.id'))
 
+    @staticmethod
+    def generate_fake(count=100):
+        from random import seed, randint
+        import forgery_py
 
+        seed()
+        user_count = User.query.count()
+        blog_count = Blog.query.count()
+        for i in range(count):
+            u = User.query.offset(randint(0, user_count - 1)).first()
+            b = Blog.query.offset(randint(0, blog_count - 1)).first()
+            c = Comment(content = forgery_py.lorem_ipsum.sentences(randint(1, 2)),
+                        create_at = forgery_py.date.date(True),
+                        disabled = False,
+                        author = u,
+                        blog = b)
+            db.session.add(c)
+            db.session.commit()
