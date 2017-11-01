d88b0fc296c556cf771e961f62ecbd5aa156d367
jyk <422925090@qq.com>
2017-11-01 15:14:22 +0800

    finish nginx and gunicorn's configration

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app = create_app('production') #os.getenv('FLASK_CONFIG') or 'default'
 manager = Manager(app)
 migrate = Migrate(app, db)
 
